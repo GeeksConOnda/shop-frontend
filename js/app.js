359122e7d742ad1d7f69116b66fd7ba3b87d2807
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-12-21 15:42:41 -0300

    static v1

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var state = {
+  category: 'todo'
+};
+
+var catalogs = document.querySelectorAll("section[data-category]");
+
+function showProducts(/** @type HashChangeEvent|string */ eventOrCategory) {
+  var newCategory = eventOrCategory;
+
+  if ('newURL' in eventOrCategory) {
+    newCategory = eventOrCategory.newURL.split("#")[1] || "todo";
+  }
+
+  if (newCategory === "todo") {
+    catalogs.forEach(function (/** @type HTMLElement */ catalog) {
+      catalog.style.display = 'block';
+      catalog.querySelector("header").style.display = "block";
+    });
+  } else {
+    catalogs.forEach(function (/** @type HTMLElement */ catalog) {
+      catalog.style.display = catalog.dataset['category'] === newCategory ? 'block' : 'none';
+      catalog.querySelector("header").style.display = "none";
+    });
+  }
+
+  state.category = newCategory;
+}
+
+function menu() {
+  var menuItems = document.querySelectorAll("nav li a");
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function (event) {
+      showProducts(menuItem.getAttribute('href').substr(1));
+    });
+  });
+}
+
+window.addEventListener("hashchange", showProducts);
+showProducts({ newURL: window.location.href });
